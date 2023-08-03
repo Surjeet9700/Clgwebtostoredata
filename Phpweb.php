5c25a95b6521c06ee2a8fd5d5b33a52ba4edfd08
SURJEET KUMAR <95761604+Surjeet9700@users.noreply.github.com>
2023-08-03 20:33:18 +0530

    Update Phpweb.php

--- Phpweb.php
+++ Phpweb.php
@@ -1,46 +1,29 @@
 <?php
-// This is the PHP script that saves the user input data into an excel file
-
-// Include the PhpSpreadsheet library
 require_once 'vendor/autoload.php';
 
-// Use the namespace of the library
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
-// Get the user input data from the AJAX request
-$name = $_POST['name'];
-$hall = $_POST['hall'];
-$email = $_POST['email'];
-$mobile = $_POST['mobile'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $spreadsheet = new Spreadsheet();
+    $sheet = $spreadsheet->getActiveSheet();
+    $sheet->setCellValue('A1', 'Name');
+    $sheet->setCellValue('B1', 'Hall Ticket No');
+    $sheet->setCellValue('C1', 'Email');
+    $sheet->setCellValue('D1', 'Mobile No');
 
-// Create a new spreadsheet object
-$spreadsheet = new Spreadsheet();
+    $row = 2;
+    $name = $_POST["name"];
+    $hallTicketNo = $_POST["hallTicketNo"];
+    $email = $_POST["email"];
+    $mobileNo = $_POST["mobileNo"];
 
-// Get the active sheet
-$sheet = $spreadsheet->getActiveSheet();
+    $sheet->setCellValue('A' . $row, $name);
+    $sheet->setCellValue('B' . $row, $hallTicketNo);
+    $sheet->setCellValue('C' . $row, $email);
+    $sheet->setCellValue('D' . $row, $mobileNo);
 
-// Set the column names
-$sheet->setCellValue('A1', 'Name');
-$sheet->setCellValue('B1', 'Hall Ticket Number');
-$sheet->setCellValue('C1', 'Email ID');
-$sheet->setCellValue('D1', 'Mobile Number');
-
-// Get the last row number
-$lastRow = $sheet->getHighestRow();
-
-// Insert the user input data into the next row
-$sheet->setCellValue('A' . ($lastRow + 1), $name);
-$sheet->setCellValue('B' . ($lastRow + 1), $hall);
-$sheet->setCellValue('C' . ($lastRow + 1), $email);
-$sheet->setCellValue('D' . ($lastRow + 1), $mobile);
-
-// Create a new writer object
-$writer = new Xlsx($spreadsheet);
-
-// Save the excel file
-$writer->save('data.xlsx');
-
-// Send a confirmation message to the user
-echo "Your data has been saved successfully in data.xlsx";
+    $writer = new Xlsx($spreadsheet);
+    $writer->save('data.xlsx');
+}
 ?>
